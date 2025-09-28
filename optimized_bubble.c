5b2ebe78a5197c154a56b1e4ee782e831f718363
zainab4087 <zainabtabassum564@gmail.com>
2025-09-29 00:08:45 +0530

    Create optimized_bubble.c

---
+++ optimized_bubble.c
@@ -0,0 +1,35 @@
+// Optimized Bubble sort
+#include <stdio.h>
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void bubble_sort(int arr[], int n){
+    int i, k, swapped;
+    for(i=0; i<n-1; i++){
+        swapped=0;
+        for(k=0; k<n-i-1; k++){
+            if(arr[k]>arr[k+1])
+            {
+                swap(&arr[k], &arr[k+1]);
+                swapped=1;
+            }
+        }
+        if(swapped==0) break;
+    }
+}
+int main() {
+int arr[10]= {12, 70, 25, 98, 74, 10, 23, 45, 56 ,85};
+int n=sizeof(arr)/sizeof(arr[0]);
+int i;
+printf("Original Array: \n");
+for(i=0; i<n; i++){
+    printf("\t%d", arr[i]);
+}
+printf("\nSorted Array: \n");
+bubble_sort(arr, n);
+for(i=0; i<n; i++){
+    printf("\t%d", arr[i]);
+}
+}
