7d0212f0389331a24933f32d47b021e282d55686
zainab4087 <zainabtabassum564@gmail.com>
2026-02-09 02:18:05 +0530

    Implement delete function for singly linked list
    Added a delete function to remove a node by value from the singly linked list. Updated main to prompt for a value to delete and display the modified list.

--- singly_linkedlist.c
+++ singly_linkedlist.c
@@ -20,6 +20,24 @@ void insert_end(int val){
     temp->next = newnode;
     }
 }
+void delete(int val){
+    struct node *temp = head, *prev = NULL;
+    if(head == NULL){
+        printf("empty");
+        return;
+    }
+    if(head->data==val){
+        head = head->next;
+        free(temp);
+        return;
+    }
+    while(temp!=NULL && temp->data!=val){
+        prev=temp;
+        temp = temp->next;
+    }
+    prev->next = temp->next;
+    free(temp);
+}
 void display(struct node *head){
     struct node *temp;
     if(temp==NULL){
@@ -35,13 +53,19 @@ void display(struct node *head){
 }
 int main(){
     int n;
+    int val;
     printf("Enter the number of values to put: \n");
     scanf("%d", &n);
     for(int i=0; i<n; i++){
         printf("Value: \n");
-        int val;
+        
         scanf("%d", &val);
         insert_end(val);
     }
+    display(head);
+    printf("\nEnter the node to be deleted: \n");
+    scanf("%d", &val);
+    delete(val);
+    printf("Modified list: \n");
     display(head);
 }
